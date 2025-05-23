e2718812b929901d6053f805ea60ce95853e5052
Waldemberg Pereira <waldemberg.pereira@outlook.com>
2025-05-23 13:36:10 -0300

    Adicionado mensagens na view login

--- usuario/views.py
+++ usuario/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate
 from django.contrib import auth
 from django.contrib import messages
+from django.contrib.messages import constants
 from .models import Usuario
 
 # Create your views here.
@@ -19,10 +20,11 @@ def login(request):
                 auth.login(request, user)
                 return redirect('/cadastro/cadastro_cliente')
             else:
+                messages.add_message(request, constants.ERROR, 'Usuário inativo no sistema!')
                 return redirect('/usuario/login') # usuário está inativo
         else:
+            messages.add_message(request, constants.ERROR, 'Usuário ou senha inválidos!')
             return redirect('/usuario/login') # senha errada
-        
 
 def logout(request):
     auth.logout(request)
